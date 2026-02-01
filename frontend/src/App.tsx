8bbccac2e31224166fdc94bb1e36f4bd08388bea
Ruben Aguilar <ragui56@wgu.edu>
2026-02-01 06:42:23 -0800

    chore done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import HomeLayout from '../layout/HomeLayout'
-import Interview from '../layout/Interview'
-import Dashboard from '../layout/Dashboard'
-import NotFound from '../layout/NotFound'
+import HomeLayout from './layout/HomeLayout'
+import Interview from './layout/Interview'
+import Dashboard from './layout/Dashboard'
+import NotFound from './layout/NotFound'
+import CaseStudy from './layout/CaseStudy'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<HomeLayout />} />
       <Route path="/interview" element={<Interview />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path='/casestudy' element={<CaseStudy />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
---
+++ frontend/src/layout/CaseStudy.tsx
@@ -0,0 +1,10 @@
+const CaseStudy = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Case Study</h1>
+      <p className="mt-4">Welcome to the case study!</p>
+    </div>
+  )
+}
+
+export default CaseStudy
