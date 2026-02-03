a8a6b9c455073b9c442d05c4d4dd0cb0a8646d01
Ruben Aguilar <ragui56@wgu.edu>
2026-02-03 03:40:02 -0800

    add mobile menu

--- frontend/src/components/main/Header.tsx
+++ frontend/src/components/main/Header.tsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import Logo from '../../assets/Logo.png'
 import Button from '../button/Button'
 import { FcGoogle } from 'react-icons/fc'
+import { IoMenu, IoClose } from 'react-icons/io5'
 
 const navLinks = [
   {to: '/interview', label: 'Interview'},
@@ -15,23 +16,46 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <header className="fixed inset-x-0 top-0 py-5 bg-white border-b">
+    <header className="fixed inset-x-0 top-0 py-5 bg-white">
       <nav className="w-11/12 mx-auto flex justify-between items-center">
         <Link to='/'>
           {<img src={Logo} alt='Debug Your Interivew logo' />}
         </Link>
-        <div className="flex items-center gap-8">
-          {navLinks.map(({to, label}) => (
-            <NavLink key={to} to={to}
-              className={({isActive}) => isActive ? 'text-orange-400' : 'hover:text-red-400'}>
-              {label}
-            </NavLink>
-          ))}
-          <Button className='border rounded-full py-2 px-4'>
-            <FcGoogle /> Sign in with Google
-          </Button>
+        <div className='hidden md:flex'>
+          <div className="flex items-center gap-8">
+            {navLinks.map(({to, label}) => (
+              <NavLink key={to} to={to}
+                className={({isActive}) => isActive ? 'text-orange-400' : 'hover:text-red-400'}>
+                {label}
+              </NavLink>
+            ))}
+            <Button className='border rounded-full py-2 px-4'>
+              <FcGoogle /> Sign in with Google
+            </Button>
+          </div>
         </div>
+
+        <button className='md:hidden p-2' onClick={() => setIsMenuOpen(prev => !prev)}>
+          {isMenuOpen ? <IoClose size={24} /> : <IoMenu size={24}/>}
+        </button>
       </nav>
+      
+      {isMenuOpen && (
+        <div className='md:hidden'>
+          <div className='container mx-auto px-6 py-6 flex flex-col gap-4'>
+            {navLinks.map(({to, label}) => (
+                <NavLink key={to} to={to} 
+                  className={({isActive}) => `text-lg py-2 ${isActive ? 'text-orange-400' : 'hover:text-red-400'}`}>
+                  {label}
+                </NavLink>
+              ))}
+              <Button className='border rounded-full py-2 px-4'>
+                <FcGoogle /> Sign in with Google
+              </Button>
+          </div>
+        </div>
+      )}
+      
     </header>
   )
 }
