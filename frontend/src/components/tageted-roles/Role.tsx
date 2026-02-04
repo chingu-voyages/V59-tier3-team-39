eec423d53d7e1dd0198364db40c579d3790f86bb
Ruben Aguilar <ragui56@wgu.edu>
2026-02-04 02:32:15 -0800

    button styling update

--- frontend/src/components/tageted-roles/Role.tsx
+++ frontend/src/components/tageted-roles/Role.tsx
@@ -18,9 +18,11 @@ const Role = ({role, description, imgSrc, altText, buttonText}: roleProps) => {
       <div className="bg-gray-200 py-25 px-12 rounded-lg">
         <img src={imgSrc} alt={altText} />
       </div>
-      <Button className="flex flex-col items-start leading-4 bg-gray-200 py-4 px-6 rounded-lg">
-        <span className="font-semibold text-xl">Debug Your Interview!</span>
-        <span>{buttonText}</span>
+      <Button className="bg-gray-200 py-4 px-6 rounded-lg">
+        <div className="flex flex-col items-start leading-4">
+          <span className="font-semibold text-xl">Debug Your Interview!</span>
+          <span>{buttonText}</span>
+        </div>
       </Button>
     </div>
   )
