2f6ac27e02bcd9e38aaaac3bbd03024f2942d9f0
Ruben Aguilar <ragui56@wgu.edu>
2026-02-04 03:23:25 -0800

    create interview header

--- frontend/src/pages/Interview.tsx
+++ frontend/src/pages/Interview.tsx
@@ -1,6 +1,6 @@
 const Interview = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-[90vh]">
       <h1 className="text-4xl font-bold">Interview</h1>
       <p className="mt-4">Welcome to the interview page!</p>
     </div>
