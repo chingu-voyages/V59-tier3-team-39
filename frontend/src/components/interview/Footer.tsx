bebc083cff5881f421cd45910be0bc087d76b0a5
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2026-02-04 14:17:36 +0100

    feat: add basic interview logic and style

--- frontend/src/components/interview/Footer.tsx
+++ frontend/src/components/interview/Footer.tsx
@@ -1,10 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-[10vh] bg-gray-800 text-white flex items-center justify-center">
-      <div className="w-full">
-        <h2 className="text-5xl text-center">Interview Footer</h2>
-      </div>
-    </footer>
+    <footer className="w-full bg-[#fafafa] border-t border-[#d7d7d7] flex items-center justify-start px-8 h-24"></footer>
   )
 }
 
--- frontend/src/components/interview/Header.tsx
+++ frontend/src/components/interview/Header.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/Logo.png'
+
 const Header = () => {
   return (
-    <header className="w-full h-[10vh] bg-gray-800 text-white flex items-center justify-center">
-      <div className="w-full">
-        <h1 className="text-5xl text-center">Interview Section</h1>
-      </div>
+    <header className="w-full bg-[#fafafa] border-b border-[#d7d7d7] flex items-center justify-between px-12 py-6 h-content">
+      <Link to="/">
+        {
+          <img
+            src={Logo}
+            alt="Debug Your Interivew logo"
+            className="max-w-68"
+          />
+        }
+      </Link>
     </header>
   )
 }
