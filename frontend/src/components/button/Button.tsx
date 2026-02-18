f78c7679c27c0693266fdd33d1f21b8f1b352b98
Ruben Aguilar <ragui56@wgu.edu>
2026-02-17 16:18:44 -0800

    update targeted roles

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
   label: string
   subLabel: string
   to: string
-  variant: 'blue' | 'light'
+  variant?: 'blue' | 'light'
 }
 
 const Button = ({ label, subLabel, to, variant = 'blue' }: ButtonProps) => {
@@ -15,15 +15,15 @@ const Button = ({ label, subLabel, to, variant = 'blue' }: ButtonProps) => {
 
   return (
     <Link to={to} className="inline-block relative">
-      <div className={`relative flex flex-col justify-center items-start px-8.5 py-2.5 lg:py-2 rounded-full border-3
-            ${isBlue ? "bg-primary-tint border-[#70ace2]" : "bg-linear-to-b from-white via-[#fafdfe] to-[#e8f4fc] border-[#bcd7f0]"}
+      <div className={`min-h-16.5 md:max-h-16.5 relative flex flex-col justify-center items-start px-8.5 py-2.5 lg:py-2 rounded-full border-3
+            ${isBlue ? "bg-primary-tint border-[#70ace2]" : "lg:px-7.5 xl:px-8.5 bg-linear-to-b from-white via-[#fafdfe] to-[#e8f4fc] border-primary-stroke-base"}
           `}
       >
-        <span className={`leading-[1.15] ${isBlue ? "text-white text-lg lg:text-xl font-medium" : "text-primary text-lg font-medium"}`}>
+        <span className={`leading-[1.15] ${isBlue ? "text-white text-lg lg:text-xl font-medium" : "text-primary-tint text-base xl:text-lg font-medium"}`}>
           {label}
         </span>
 
-        <span className={`leading-[1.35] ${ isBlue ? "text-white/90 text-sm lg:text-base" : "text-sm" }`}>
+        <span className={`leading-[1.35] text-sm ${ isBlue ? "text-white/90  lg:text-base" : "md:leading-[1.15] xl:text-base" }`}>
           {subLabel}
         </span>
 
--- frontend/src/components/tageted-roles/TargetedRoles.tsx
+++ frontend/src/components/tageted-roles/TargetedRoles.tsx
@@ -1,52 +1,98 @@
 import Role from "./Role"
-import roleImg from '../../assets/homeImages/roleImg.png'
+import { useState } from "react"
+
 
 const roleData = [
   {
-    role: 'Web Developers',
-    description: 'Review modern framework concepts and technical terminology: never "blank" on a React question again.',
-    imgSrc: roleImg,
-    altText: 'role image',
+    title: {
+      role: 'Web', 
+      type: 'Developers'
+    },
+    content: {
+      lead: 'Review modern framework concepts',
+      body: `and technical terminology: don’t "blank" on that React question.`
+    },
     button: {
       label: 'Debug Your Interview!',
       subLabel: 'Simulate a front-end interview.',
-      to: '/interview'
+      to: '/interview',
+      variant: 'light' as const
     }
   },
   {
-    role: 'UX Designers',
-    description: 'Practice articulating your methodology and the "why" behind your design decisions with confidence.',
-    imgSrc: roleImg,
-    altText: 'role image', 
+    title: {
+      role: 'UX', 
+      type: 'Designers'
+    },
+    content : {
+      lead: 'Practice articulating your methodology',
+      body: 'and the "why" behind your design decisions with confidence.',
+    },
     button: {
       label: 'Debug Your Interview!',
       subLabel: 'Dry run an interview for a UX role.',
-      to: '/interview'
+      to: '/interview',
+      variant: 'light' as const
     }
   },
   {
-    role: 'Python Developers',
-    description: `Move beyond the syntax with structured practice on data structures and\nalgorithms.`,
-    imgSrc: roleImg,
-    altText: 'role image',
+    title: {
+      role: 'Python', 
+      type: 'Developers'
+    },
+    content: {
+      lead: 'Move beyond the syntax',
+      body: `with structured practice on data structures and algorithms.`,
+    },
     button: {
       label: 'Debug Your Interview!',
       subLabel: 'Build your skills and your confidence.',
-      to: '/interview'
+      to: '/interview',
+      variant: 'light' as const
     }
   },
-
 ]
 
 const TargetedRoles = () => {
+
+  const [isActive, setIsActive] = useState(0)
+
   return (
-    <div className="w-13/14 mx-auto py-12">
-      <div className="grid md:grid-cols-3 gap-14">
-        {roleData.map(card => ( 
-          <Role key={card.role} {...card} />
+    <section className="flex flex-col mb-6 items-center">
+      <div role='tablist' className="md:hidden flex justify-center">
+        {roleData.map((card, index) => (
+          <div 
+            key={card.title.role}
+            role='tab'
+            aria-selected={index === isActive}
+            onClick={() => setIsActive(index)} 
+            className={`
+              min-w-30 flex flex-col z-10 -mb-px px-3.75 py-1.75 font-bold leading-[1.10] border-t border-r 
+              ${index === isActive ? 'bg-primary-bg-gradient-active' : 'bg-primary-bg-gradient'} ${card.title.role === 'Web' ? 'border-l': 'border-l-0'} 
+              rounded-t-lg border-primary-stroke-tint cursor-pointer
+            `}
+          >
+            <span>{card.title.role}</span>
+            <span className="text-secondary-base">{card.title.type}</span>
+          </div>
         ))}
       </div>
-    </div>
+      <div className="border-t md:border-0 border-primary-stroke-base">
+        <div 
+          role='tabpanel'
+          aria-label={`${roleData[isActive]?.title.role} content`}
+          className="md:hidden w-full py-6 px-6">
+          {roleData.map((card, index) => (
+            isActive === index ? <Role key={card.title.role} {...card} /> : null 
+          ))}
+        </div>
+        <div className="hidden md:flex gap-6 md-lg:gap-8 md-lg-x:gap-9 lg:gap-9 xl:gap-14 w-full p-8 lg:p-10 xl:p-12">
+          {roleData.map(card => (
+            <Role key={card.title.role} {...card} /> 
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
